7f39c11346b6ba045058bb2161ec9c35fde76bb6
Dominik <mail@meyerdominik.de>
2020-12-19 10:23:46 +0100

    first test

---
+++ Bluetooth/Device.py
@@ -0,0 +1,4 @@
+class Device:
+    Group = ""
+    MAC = ""
+    Name = ""
---
+++ Bluetooth/bluetoothAPI.py
@@ -0,0 +1,12 @@
+import os
+
+
+def __SendCommand(command):
+    return os.system(command)
+
+def GetDevices():
+    rtn = list()
+
+    AllDevices = __SendCommand("echo \"devices\\nquit\" | bluetoothctl")
+
+    print AllDevices
